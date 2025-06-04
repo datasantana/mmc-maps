2fd42bae4d2a29f313ea97f8b1e89b69eca412c3
datasantana <juan.linares@geostudio.com.co>
2025-06-04 08:44:37 -0500

    Add Map Template view and update navigation links

---
+++ src/components/MapTemplate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="mapContainer" class="map"></div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl';
+
+export default {
+  name: 'map-template',
+  mounted() {
+    // Replace with your actual Mapbox access token.
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZ2Vvc3R1ZGlvIiwiYSI6ImNrYndtazR3OTA5cmEycHFxcTl4MWs1aHgifQ.nJvUs7kTlQCzb_-Fda2RSg';
+    this.map = new mapboxgl.Map({
+      container: this.$refs.mapContainer,
+      style: 'mapbox://styles/geostudio/cmbh999uh001901qt6te20agp', // Map style URL.
+      center: [-74.006, 40.7128], // Starting position [lng, lat].
+      zoom: 13 // Starting zoom level.
+    });
+  }
+};
+</script>
+
+<style scoped>
+.map {
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ src/views/MapTemplateView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="map">
+    <MapTemplate />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MapTemplate from '@/components/MapTemplate.vue'
+
+export default {
+  name: 'MapTemplateView',
+  components: {
+    MapTemplate
+  }
+}
+</script>
