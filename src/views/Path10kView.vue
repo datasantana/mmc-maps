09ab816e71d395787b61a006fa57c18895310a5a
datasantana <juan.linares@geostudio.com.co>
2025-06-24 21:08:44 -0500

    Add path components and views for 5k, 10k, and 21k routes
    - Created Path10k.vue, Path21k.vue, and Path5k.vue components for rendering respective paths on the map.
    - Implemented Path10kView.vue, Path21kView.vue, and Path5kView.vue to serve as views for the new components.
    - Updated router to include routes for 5k, 10k, and 21k paths.
    - Integrated Mapbox GL JS for interactive map visualization with markers and animations.

---
+++ src/views/Path10kView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="map">
+    <Path10k />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Path10k from '@/components/Path10k.vue'
+export default {
+  name: 'Path10kView',
+  components: {
+    Path10k
+  }
+}
+</script>
---
+++ src/views/Path5kView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="map">
+    <Path5k />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Path5k from '@/components/Path5k.vue'
+export default {
+  name: 'Path5kView',
+  components: {
+    Path5k
+  }
+}
+</script>
