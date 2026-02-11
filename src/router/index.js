50ea920805f206d57ae98171b3497842eeedf7ab
Juan David Linares <juan.linares@geostudio.com.co>
2026-02-11 18:36:26 -0500

    feat: implement 404 redirection handling and enhance navigation with smooth scrolling

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MapTemplateView from '@/views/MapTemplateView.vue'
 import Path21kView from '@/views/Path21kView.vue'
@@ -43,7 +43,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
